6a53af41514f28f4cce74db42e3d33393b157772
duqrldudgns <48309387+duqrldudgns@users.noreply.github.com>
2020-07-07 20:37:26 +0900

    Update copy.c

--- copy.c
+++ copy.c
@@ -1,7 +1,12 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <string.h>
+
 int main(int argc, char *argv[])
 {
         if (argc < 3 ) {
-                //printf("Usage : %s <filename1> <filename2>\nor\n", argv[0]);
                 printf("Usage : %s <filename1> .. <filename N> <directoryname>\n", argv[0]);
                 return 0;
         }
@@ -14,15 +19,17 @@ int main(int argc, char *argv[])
         for(int i=1;i<argc-1;i++)
         {
                 memset(dir, 0, 50);
-                strcat(dir,"./");
-                strcat(dir,argv[argc-1]);
-                strcat(dir,"/");
-                strcat(dir,argv[i]);
+                sprintf(dir, "./%s/%s",argv[argc-1] ,argv[i]);
 
                 //printf("int : %d, dir : %s \n",i, dir);
 
                 fd = fopen(argv[i], "r");
-                dest = fopen(dir, "w");
+                if((dest = fopen(dir, "w")) == NULL) {
+                        printf("%s directory open failed \n",argv[i+1]);
+                        printf("Before running, the target directory must be created\n");
+                        printf("Usage : %s <filename1> .. <filename N> <directoryname>\n", argv[0]);
+                        return 0;
+                }
 
                 while ( len = fread(buf, 1, 256, fd) )
                 {
